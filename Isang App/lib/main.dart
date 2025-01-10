6b49b03fbefe2eee92aed73820c5f216015fd303
danji404 <157474285+danji404@users.noreply.github.com>
2025-01-10 20:38:18 +0900

    타이틀 수정

--- Isang App/lib/main.dart
+++ Isang App/lib/main.dart
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: '할일 관리 앱',
+      title: 'Isang made by Jihwan',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
